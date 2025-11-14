4bb44d3004a663d5ec13ea0e06838035a7089202
SYNC TECH Solutions <synctechire@gmail.com>
2025-11-14 19:31:09 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -60,6 +60,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.linkedin.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async headers() {
