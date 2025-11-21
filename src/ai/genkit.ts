020dffc4deab54da136f09c485de12e17629eec6
SYNC TECH Solutions <synctechire@gmail.com>
2025-11-21 17:40:29 +0000

    let me make one thing clear gemini-1.5-flash does not exist anymore and

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,9 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      apiVersion: 'v1',
-    }),
+    googleAI(),
   ],
   // Optional. Specify a default model.
   model: 'gemini-1.5-flash',
